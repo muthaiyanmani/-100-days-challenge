bf98c23441d6505852a8e28f7f2f6653a1aaeef8
Muthaiyan Mani <muthaiyanmani00@gmail.com>
2021-11-23 19:24:18 +0530

    worked on user defined function

---
+++ 16atoi.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int atoi(string str)
+{
+    long int strToInt = 0;
+    if (!(str.length() <= 10)) {
+        cout << "Only 10 digit is allowed" << endl;
+        return 0;
+    }
+    else{
+        for (int i = 0; str[i]; i++){
+            if (str[i] >= 48 && str[i] <= 57)
+                strToInt = (strToInt * 10) + (int(str[i]) - 48);
+            else{
+                cout << "Enter numbers in string!" << endl;
+                return 0;
+            }
+        }
+    }
+    return strToInt;
+}
+
+int main()
+{
+    string name = "-10";
+    cout << atoi(name);
+    return 0;
+}
