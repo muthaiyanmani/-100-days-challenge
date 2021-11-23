8ae195153659600f538a80177b3132cf1481b38c
Muthaiyan Mani <muthaiyanmani00@gmail.com>
2021-11-23 18:53:12 +0530

    fixed character input in IP address

--- 15IpAddress.cpp
+++ 15IpAddress.cpp
@@ -5,30 +5,39 @@ using namespace std;
 
 bool isValidIp(string ip)
 {
-    if(ip[0]=='0')
+    if (ip[0] == '0' || !(ip.length() <= 15))
         return false;
 
-    //first three octates
-    int initialValue = 0;
-    short int count = 0;
-    for (int i = 0; ip[i]; i++)
-        if (ip[i] == '.')
-        {
-            string splitted = ip.substr(initialValue, i);
-            initialValue = ++i;
-            int strToInt = atoi(splitted.c_str());
-            if (!(0 <= strToInt && strToInt <= 255))
+    else
+    {
+        //first three octates
+        int initialValue = 0;
+        short int count = 0;
+        for (int i = 0; ip[i]; i++)
+            //check whether number or character
+            if ((ip[i] >= 48 && ip[i] <= 57) || ip[i] == 46)
+            {
+                if (ip[i] == '.')
+                {
+                    string splitted = ip.substr(initialValue, i);
+                    initialValue = i + 1;
+                    int strToInt = atoi(splitted.c_str());
+                    if (!(0 <= strToInt && strToInt <= 255))
+                        return false;
+                    count++;
+                }
+            }
+            else
                 return false;
-            count++;
-        }
 
-    //last one octates
-    string splitted = ip.substr(initialValue, ip.length());
-    int strToInt = atoi(splitted.c_str());
+        //last one octates
+        string splitted = ip.substr(initialValue, ip.length());
+        int strToInt = atoi(splitted.c_str());
 
-    if (!(0 <= strToInt && strToInt <= 255) || !(count == 3))
-        return false;
-    return true;
+        if (!(0 <= strToInt && strToInt <= 255) || !(count == 3) || !(splitted.length()))
+            return false;
+        return true;
+    }
 }
 
 string getInput()
@@ -45,27 +54,30 @@ string getInput()
 int main()
 {
     string ip = getInput();
-    cout << "Get input Successfully!"<< endl;
-    
-    for(short int i=0;ip[i];i++)
-        if(ip[i]=='.'){
+    cout << "Get input Successfully!" << endl;
+
+    for (short int i = 0; ip[i]; i++)
+        if (ip[i] == '.')
+        {
             string splitted = ip.substr(0, i);
             int strToInt = atoi(splitted.c_str());
-            
-            if(1<=strToInt && strToInt<=126){
+
+            if (1 <= strToInt && strToInt <= 126)
                 cout << "Class A" << endl;
-            }else if(ip=="127.0.0.1"){
+            else if (ip == "127.0.0.1")
                 cout << "Localhost" << endl;
-            }else if(128<=strToInt && strToInt<=191){
+            else if (128 <= strToInt && strToInt <= 191)
                 cout << "Class B" << endl;
-            }else if(192<=strToInt && 223>=strToInt){
-                cout << "Class C"<<endl;
-            }else{
-                cout << "Future reserved"<<endl;
-            }
+            else if (192 <= strToInt && 223 >= strToInt)
+                cout << "Class C" << endl;
+            else if (224 <= strToInt && 239 >= strToInt)
+                cout << "Class D" << endl;
+            else if (240 <= strToInt && 255 >= strToInt)
+                cout << "Class E" << endl;
+            else
+                cout << "Something went wrong!" << endl;
             break;
         }
-
 
     return 0;
 }
